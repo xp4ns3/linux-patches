48d94be3afc9f4219c0f439e6a1796a65c8d05eb
Fabio M. De Francesco <fmdefrancesco@gmail.com>
2021-11-14 08:56:05 +0100

    Update fabio-usb-qemu.sh

--- qemu/fabio-usb-qemu.sh
+++ qemu/fabio-usb-qemu.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-KERNEL=.
+KERNEL=/home/xp4ns3/git/kernels/staging/
 DEVICE_NAME="ASUSTek Computer, Inc. Realtek 8188EUS [USB-N10 Nano]"
 BUS=$(lsusb | grep "$DEVICE_NAME" | awk '{print $2}')
 DEVICE=$(lsusb | grep "$DEVICE_NAME" | awk '{print $4}' | head -c -2 | sed -r 's/^0+|0+$//g')
 
 
 qemu-system-x86_64 \
-	-m 2G \
-	-smp 4 \
+	-m 4G \
+	-smp 2 \
 	-kernel $KERNEL/arch/x86/boot/bzImage \
+	-initrd /boot/init
 	-append "console=ttyS0 root=/dev/mapper/cr_root rw panic_on_warn=off earlyprintk=serial" \
 	-drive file=./usb.img,format=raw \
 	-net user,host=10.0.2.10,hostfwd=tcp:127.0.0.1:10021-:22 \
