f399955e249ac6558958dec0c52bd8849509134d
Fabio M. De Francesco <fmdefrancesco@gmail.com>
2021-11-14 08:30:51 +0100

    Move and rename files to qemu/ and syzkaller/

--- qemu/fabio-usb-qemu.sh
+++ qemu/fabio-usb-qemu.sh
@@ -8,9 +8,9 @@ DEVICE=$(lsusb | grep "$DEVICE_NAME" | awk '{print $4}' | head -c -2 | sed -r 's
 
 qemu-system-x86_64 \
 	-m 2G \
-	-smp 3 \
+	-smp 4 \
 	-kernel $KERNEL/arch/x86/boot/bzImage \
-	-append "console=ttyS0 root=/dev/sda rw panic_on_warn=off earlyprintk=serial" \
+	-append "console=ttyS0 root=/dev/mapper/cr_root rw panic_on_warn=off earlyprintk=serial" \
 	-drive file=./usb.img,format=raw \
 	-net user,host=10.0.2.10,hostfwd=tcp:127.0.0.1:10021-:22 \
 	-net nic,model=e1000 \
